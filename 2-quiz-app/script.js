71df9bedb4703799f2de13a66d600fdb8e9deebb
OndrejValla <ondrej.valla@gmail.com>
2022-01-23 18:34:39 +0000

    Added deselectAnswer function

--- 2-quiz-app/script.js
+++ 2-quiz-app/script.js
@@ -35,18 +35,24 @@ const quizData = [{
     correct: 'b',
 }];
 
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
+
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
+
     const currentQuizData = quizData[currentQuiz];
+
     questionEl.innerHTML = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
@@ -54,14 +60,38 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d;
 }
 
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
 
 submitBtn.addEventListener('click', () => {
-    currentQuiz++;
+    const answer = getSelected();
 
-    if (currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
-        //TO DO show results
-        alert('You have finished!')
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            //TO DO show results
+            alert('You have finished!')
+        }
     }
 });
